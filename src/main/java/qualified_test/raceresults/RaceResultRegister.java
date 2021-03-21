b5f6a6f758481d06753d54f65d479d2530ce5fef
huszarpp <huszarpp@gmail.com>
2021-03-21 22:18:54 +0100

    Create getAllResults() NOT READY, getMissingCompetitors() NOT READY

--- src/main/java/qualified_test/raceresults/RaceResultRegister.java
+++ src/main/java/qualified_test/raceresults/RaceResultRegister.java
@@ -1,6 +1,8 @@
 package qualified_test.raceresults;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RaceResultRegister {
 
@@ -46,14 +48,55 @@ public class RaceResultRegister {
         int lastResult = 0;
 
         try (Connection connection = getConnection()) {
-            String sql = "SELECT MAX(id), result FROM race_result WHERE competitor_id = ?;";
+            String sql = "SELECT MAX(id), competitor_id, result FROM race_result GROUP BY competitor_id HAVING competitor_id = ?;";
             PreparedStatement pst = connection.prepareStatement(sql);
             pst.setInt(1, competitorId);
             ResultSet rst = pst.executeQuery();
+            rst.next();
             lastResult = rst.getInt("result");
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
         return lastResult;
+    }
+
+    public List<Integer> getAllResults(int competitorId) {
+
+        List<Integer> resultList = new ArrayList<>();
+
+        try (Connection connection = getConnection()) {
+            String sql = "SELECT result FROM race_result WHERE competitor_id = ?;";
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setInt(1, competitorId);
+            ResultSet rst = pst.executeQuery();
+            while (rst.next()) {
+                resultList.add(rst.getInt("result"));
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return resultList;
+    }
+
+    public List<String> getMissingCompetitors() {
+
+        List<String> missingCompetitors = new ArrayList<>();
+
+        try (Connection connection = getConnection()) {
+            String sql = "" +
+                    "SELECT competitor_name, race_result.id " +
+                    "FROM competitor " +
+                    "INNER JOIN race_result ON competitor.id = race_result.competitor_id " +
+                    "WHERE competitor_id = NULL;";
+            PreparedStatement pst = connection.prepareStatement(sql);
+            ResultSet rst = pst.executeQuery();
+            while (rst.next()) {
+                missingCompetitors.add(rst.getString("competitor_name"));
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+
+        return missingCompetitors;
     }
 }
